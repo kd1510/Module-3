aabf42c5a12ef873204649ed3dbec34d7f3f6992
Krishan Davda <krishan.davda@simplybusiness.co.uk>
2026-03-19 16:24:01 +0000

    start adding metal ops

---
+++ minitorch/metal_ops.py
@@ -0,0 +1,8 @@
+"""
+TODO: Write metal kernels to be able to use as a backend for the library when running on a mac.
+
+https://ml-explore.github.io/mlx/build/html/dev/custom_metal_kernels.html
+
+Apple's library above gives the ability to compile custom metal shaders.
+
+"""
